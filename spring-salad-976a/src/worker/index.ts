a1826d066a69e24da2c6c8d00e26b30c92be24e8
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2025-11-23 14:58:51 -0600

    cloudfare template + generate random color

---
+++ spring-salad-976a/src/worker/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+const app = new Hono<{ Bindings: Env }>();
+
+app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/random_color", (c) => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return c.json({ color: `rgb(${red}, ${green}, ${blue})` });
+})
+
+export default app;
